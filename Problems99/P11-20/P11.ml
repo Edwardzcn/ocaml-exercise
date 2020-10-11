16347e974d1e0d2f363987554eec74cd4e66492e
Edwardzcn <edwardzcn98@gmail.com>
2020-10-12 00:46:19 +0800

    feat: add P11 and P12

--- Problems99/P11-20/P11.ml
+++ Problems99/P11-20/P11.ml
@@ -1,6 +1,1 @@
-let length list  = 
-  let rec length_2parm cnt  = function
-  | [] -> cnt
-  | head::tail -> length_2parm  (cnt+1) tail
-  in length_2parm  0 list;;
-length [1;2;3;4]                (* test *)
+(* duplicate to many and one to one  *)
