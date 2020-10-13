3374e0c4b524c12f7ba7ec54de6dbf690dd7fad7
Edwardzcn <edwardzcn98@gmail.com>
2020-10-13 11:50:51 +0000

    feat: add P46-47 and P48
    Learn to form the truth tables of logical expressions.

--- Problems99/P31-40/P39.ml
+++ Problems99/P31-40/P39.ml
@@ -14,6 +14,14 @@ let rec  all_primes a b =
 
 all_primes 3 1999;;
 
+all_primes 3 2000000;;
 
-let rec all_primes a b =
-  let range a b 
+let rec all_primes_tail a b =   (* My tail recursive  *)
+  let rec aux low high acc =
+    if low > high then acc
+    else if is_prime_ref low then aux (low+1) high (low::acc)
+    else aux (low+1) high acc
+  in List.rev (aux  a b []);;
+
+
+(* want Eratosthenes but I can't manage it *)
